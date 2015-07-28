d861c5d916136a395e64241bc6409b93e41ca7e9
Richard Möhn <richard.moehn@posteo.de>
2015-07-28 14:11:49 +0900

    Add conversion to Grenada and packaging

--- src/lein_datadoc/core.clj
+++ src/lein_datadoc/core.clj
@@ -1,26 +1,64 @@
 (ns lein-datadoc.core
   (:require [clojure.java.io :as io]
+            [grenada
+             [core :as gren-core]
+             [exporters :as exporters]]
+            [grimoire.api.fs :as api.fs]
             grimoire.doc
-            [plumbing.core :refer [safe-get]]))
+            [jolly.core :as jolly]
+            [plumbing
+             [core :refer [fnk safe-get <-]]
+             [graph :as graph]]))
 
 ;;; Note: This probably has to evolve to a full plugin. Having to provide
 ;;;       entries from project.clj through :aliases is already tedious, but
 ;;;       still doable. For the deployment we'll need Leiningen functionality,
 ;;;       though.
 
-;; TODO: Make these configurable in project.clj.
-(def config {:grim-out "grimoire-data"})
+;; TODO: Make these configurable in project.clj. (RM 2015-07-27)
+(defn get-config [target-path]
+  ((graph/eager-compile
+    {;; Name of directory in target-path where we put everything
+     :grenada-dir (fnk [target-path] (io/file target-path "grenada"))
+
+     ;; Where the Grimoire data should be stored
+     :grim-out (fnk [grenada-dir] (io/file grenada-dir "grimoire-data"))
+
+     ;; Configuration object for lib-grimoire calls
+     :grim-config (fnk [grim-out]
+                    (apply api.fs/->Config
+                           (map str (repeat 3 grim-out))))
+
+     ;; Where the raw Grenada data should be stored
+     :grenada-out (fnk [grenada-dir] (io/file grenada-dir "grenada-data"))})
+   {:target-path target-path}))
+
 
 (defn -main
   "
 
-  Arguments up to & to be supplied directly by Leiningen."
+  Arguments up to & to be supplied directly by Leiningen.
+
+  Note that the JAR produced will have the same group, artifact and version as
+  the main project JAR, but a different classifier."
   [group artifact version target-path & source-paths]
-  (grimoire.doc/-main group
-                      artifact
-                      version
-                      source-paths
-                      "source"
-                      "clj"
-                      (str (io/file target-path
-                                    (safe-get config :grim-out)))))
+  (let [config (get-config target-path)]
+    (grimoire.doc/-main group
+                        artifact
+                        version
+                        source-paths
+                        "--clobber"
+                        "true"
+                        "source"
+                        "clj"
+                        (str (safe-get config :grim-out)))
+
+    (->> (jolly/read-all-things (safe-get config :grim-config))
+         (jolly/grim-ts->gren-ts-with-bars (safe-get config :grim-config))
+         (<- (exporters/fs-hier (safe-get config :grenada-out))))
+
+    (gren-core/jar-from-files (safe-get config :grenada-out)
+                              (safe-get config :grenada-dir)
+                              {:group group
+                               :artifact artifact
+                               :version version})))
