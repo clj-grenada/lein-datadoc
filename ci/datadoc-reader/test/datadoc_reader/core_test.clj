daff04671fc02e7991141876d5f82331406aa020
Richard Möhn <richard.moehn@posteo.de>
2015-08-16 11:47:54 +0900

    CI: Test lib-grenada Datadoc JAR, with clojure.test
     - Change the ad-hoc asserts into proper clojure.test tests.
    
     - Do extraction and tests on lib-grenada in addition to
       prismatic/schema. – lib-grenada uses features of Things that no one
       else uses, so it makes sense to include it.

--- ci/datadoc-reader/test/datadoc_reader/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns datadoc-reader.core-test
-  (:require [clojure.test :refer :all]
-            [datadoc-reader.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
