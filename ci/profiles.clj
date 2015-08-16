e01b4e2110df87a0849811eda97b2f02235c7ac2
Richard Möhn <richard.moehn@posteo.de>
2015-08-16 10:09:16 +0900

    ci: Remove deployment to pseudo-repo
    It doesn't work. It asks for authentication for a file:///. Not sure how
    to understand
    https://github.com/technomancy/leiningen/blob/stable/doc/DEPLOY.md in
    that regard. It's not so important. Leave it out.

--- ci/profiles.clj
+++ ci/profiles.clj
@@ -1,4 +1,3 @@
-{:user {:aliases {"datadoc" ["with-profile" "datadoc" "datadoc"]}
-        :deploy-repositories [["pseudo-repo" "file:///tmp/pseudo-repo"]]}
+{:user {:aliases {"datadoc" ["with-profile" "datadoc" "datadoc"]}}
  :datadoc {:dependencies [[org.clojars.rmoehn/lein-grim "0.3.10"]]
            :plugins [[org.clj-grenada/lein-datadoc "1.0.0-rc.1"]]}}
