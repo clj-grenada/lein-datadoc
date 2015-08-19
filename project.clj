fe07fd05b1f703437d9ab616e3397dff8955a66e
Richard Möhn <richard.moehn@posteo.de>
2015-08-19 15:31:12 +0900

    Bump lib-grenada to 1.0.0-rc.2

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clj-grenada/jolly "1.0.0-rc.2"]
-                 [org.clj-grenada/lib-grenada "1.0.0-rc.1"]
+                 [org.clj-grenada/lib-grenada "1.0.0-rc.2"]
                  [org.clojars.rmoehn/lein-grim "0.3.10"]
                  [prismatic/plumbing "0.4.0"]])
