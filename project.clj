e2b33d7015931cbaa50e414dbeb88988785077f2
Richard Möhn <richard.moehn@posteo.de>
2015-08-21 17:50:07 +0900

    Bump to lib-grenada 1.0.0-rc.4, jolly 1.0.0-rc.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clj-grenada/jolly "1.0.0-rc.2"]
-                 [org.clj-grenada/lib-grenada "1.0.0-rc.2"]
+                 [org.clj-grenada/jolly "1.0.0-rc.3"]
+                 [org.clj-grenada/lib-grenada "1.0.0-rc.4"]
                  [org.clojars.rmoehn/lein-grim "0.3.10"]
                  [prismatic/plumbing "0.4.0"]])
